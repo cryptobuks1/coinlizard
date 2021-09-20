c828052e8e0de7498b0bff0d47c914277ecfaa00
Emile Choghi <emchoghi@gmail.com>
2021-09-20 16:03:57 -0700

    refactor: PercentChange component in Coin table row

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -16,6 +16,10 @@ function Coin({
     // parse image path to get unique coin ID that gives us access to the 7 day chart
     const coinChartID = image.split('images/')[1].split('/large')[0];
 
+    function PercentChange({ n, name }) {
+        return <div className={`${n >= 0 ? 'positive' : 'negative'} ${name}`}>{n ? `${n.toFixed(1)}%` : 0}</div>;
+    }
+
     return (
         <div className="coin__container">
             <div className="coin__rank">
@@ -27,15 +31,9 @@ function Coin({
                 <div>{symbol ? symbol.toUpperCase() : ''}</div>
             </div>
             <div className="coin__price">{formatPrice(current_price)}</div>
-            <div className={`${price_change_percentage_1h_in_currency >= 0 ? 'positive' : 'negative'} coin__hr`}>
-                {price_change_percentage_1h_in_currency ? `${price_change_percentage_1h_in_currency.toFixed(1)}%` : 0}
-            </div>
-            <div className={`${price_change_percentage_24h_in_currency >= 0 ? 'positive' : 'negative'} coin__24hr`}>
-                {price_change_percentage_24h_in_currency ? `${price_change_percentage_24h_in_currency.toFixed(1)}%` : 0}
-            </div>
-            <div className={`${price_change_percentage_7d_in_currency >= 0 ? 'positive' : 'negative'} coin__week`}>
-                {price_change_percentage_7d_in_currency ? `${price_change_percentage_7d_in_currency.toFixed(1)}%` : 0}
-            </div>
+            <PercentChange n={price_change_percentage_1h_in_currency} name="coin__hr" />
+            <PercentChange n={price_change_percentage_24h_in_currency} name="coin__24hr" />
+            <PercentChange n={price_change_percentage_7d_in_currency} name="coin__week" />
             <div className="coin__volume">{total_volume ? `$${numberWithCommas(total_volume)}` : 0}</div>
             <div className="coin__cap">{market_cap ? `$${numberWithCommas(market_cap)}` : 0}</div>
             <div className="coin__chart">
