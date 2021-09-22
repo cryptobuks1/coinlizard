9ff7ae20ccec59e425748722f03fbd0c89a3854b
Emile Choghi <emchoghi@gmail.com>
2021-09-22 11:21:52 -0700

    fix: add MiniCSSExtractPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
+    plugins: [new MiniCssExtractPlugin()],
     module: {
         rules: [
             {
